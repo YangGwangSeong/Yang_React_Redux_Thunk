39cb3f4ca46d8bd97be443759aaac2f4c665866d
yanggwangseong <rhkdtjd_12@naver.com>
2021-06-25 15:43:29 +0900

    react-thunk 설치 및 thunk 함수 생성 및 사용

--- client/src/containers/CounterContainer.jsx
+++ client/src/containers/CounterContainer.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Counter from "../components/Counter"; //사용할 컴포넌트 불러오기.
 import { useSelector, useDispatch } from "react-redux";
-import { increase, decrease } from "../modules/counter";
+import { increase, decrease, increaseAsync, decreaseAsync } from "../modules/counter";
 
 function CounterContainer(){
     const number = useSelector(state => state.counter);
     const dispatch = useDispatch();
 
     const onIncrease = () => {
-      dispatch(increase());  
+      dispatch(increaseAsync()); //thunk 함수 사용
     };
     const onDecrease = () => {
-        dispatch(decrease());
+        dispatch(decreaseAsync()); //thunk 함수 사용
     };
 
     return (
--- client/src/index.js
+++ client/src/index.js
@@ -11,9 +11,14 @@ import logger from "redux-logger";
 //npm install redux-logger
 //npm install redux-devtools-extension
 // 콤바인리듀서로 여러개의 리듀서 등록하기
+
+
+import ReduxThunk from "redux-thunk";
+// redux-thunk 사용하기 위해 함수를 디스패치 가능. dispatch 와 getState를 파라미터로 받아와줘야함.
+
 const store = createStore(
   rootReducer, 
-  composeWithDevTools(applyMiddleware(logger))
+  composeWithDevTools(applyMiddleware(ReduxThunk,logger))
 );
 
 ReactDOM.render(
