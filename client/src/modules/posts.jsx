912cda2fbe73417eabca459b0d97efd23963ac01
yanggwangseong <rhkdtjd_12@naver.com>
2021-06-28 11:42:14 +0900

    Api 재로딩 방지 리팩토링

--- client/src/modules/posts.jsx
+++ client/src/modules/posts.jsx
@@ -1,5 +1,10 @@
 import * as postsAPI from "../api/posts"; // api/posts 안의 함수 모두 불러오기
-import { createPromiseThunk, reducerUtils, handleAsyncActions } from '../lib/asyncUtils';
+import { createPromiseThunk, 
+    reducerUtils, 
+    handleAsyncActions,
+    createPromiseThunkById,
+    handleAsyncActionsById
+ } from '../lib/asyncUtils';
 /* 
     프로미스를 다루는 리덕스 모듈을 다룰때 주의사항
 
@@ -29,7 +34,7 @@ const initialState = {
 const initialState = {
     
     posts : reducerUtils.initial(),
-    post : reducerUtils.initial()
+    post : {}
 }
 
 // 포스트 여러개 조회하기
@@ -41,6 +46,9 @@ const GET_POSTS_ERROR = "GET_POSTS_ERROR"; // 요청 실패
 const GET_POST = "GET_POST";
 const GET_POST_SUCCESS = "GET_POST_SUCCESS";
 const GET_POST_ERROR = "GET_POST_ERROR";
+
+// 포스트 비우기
+const CLEAR_POST = "CLEAR_POST";
 
 /*
 // thunk 를 사용 할 때, 꼭 모든 액션들에 대하여 액션 생성함수를 만들 필요는 없습니다.
@@ -68,20 +76,27 @@ export const getPost = (id) => async dispatch => {
 */
 
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
+
+export const clearPost = () => ({ type : CLEAR_POST });
 
 export default function posts(state = initialState, action){
     switch (action.type){
         case GET_POSTS:
         case GET_POSTS_SUCCESS:
         case GET_POSTS_ERROR:
-            const postsReducer = handleAsyncActions(GET_POSTS, "posts");
+            const postsReducer = handleAsyncActions(GET_POSTS, "posts", true);
             return postsReducer(state,action);
         case GET_POST:
         case GET_POST_SUCCESS:
         case GET_POST_ERROR:
-            const postReducer = handleAsyncActions(GET_POST, "post");
+            const postReducer = handleAsyncActionsById(GET_POST, "post", true);
             return postReducer(state,action);
+        case CLEAR_POST:
+            return {
+                ...state,
+                post: reducerUtils.initial()
+            };
         default : 
             return state;
     };
